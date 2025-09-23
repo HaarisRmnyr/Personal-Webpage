a6e01df4ee4c5472b358a41523f06b3a380118a0
hrs201 <haarisrahmanyar@gmail.com>
2025-09-23 14:57:10 -0400

    upadated img paths

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -10,7 +10,7 @@ function About() {
     return(
         <div className="flex flex-col justify-center items-center">
             <h1 className="text-3xl sm:text-4xl md:text-5xl text-white text-center mt-40">About Me</h1>
-            <img src="/images/Placeholder.png"alt="Placeholder-me" className="w-10 sm:w-1/4 md:w-20 md: mt-20"/>
+            <img src="./images/Placeholder.png"alt="Placeholder-me" className="w-10 sm:w-1/4 md:w-20 md: mt-20"/>
 
 
             <div className="flex flex-col items-center justify-center md:justify-start mt-20 mb-20 md:flex-row">
@@ -20,7 +20,7 @@ function About() {
                 a combonation of React and Tailwindcss aswell as HTML and CSS for frontend development. For backend, I'm able to effectivley use Express js, Node.js 
                 , and MongoDB. My passion for cybersecurity has also given me a familiarity with Kali linux and Virtualbox, along with tools like burpsuite, Metasploit, and Splunk.</p>
 
-                <img src="/images/About-section-icons.jpg"alt="My company logo" className="w-64 sm:w-1/2 md:w-1/3 md:mb-72 md:ml-64"/>
+                <img src="./images/About-section-icons.jpg"alt="My company logo" className="w-64 sm:w-1/2 md:w-1/3 md:mb-72 md:ml-64"/>
 
 
 
