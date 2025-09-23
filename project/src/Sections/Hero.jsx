2c4ea7a827592d16bde0414d18379e6c15ebc514
hrs201 <haarisrahmanyar@gmail.com>
2025-09-23 03:28:01 -0400

    Added Animation to Hero.jsx - Created Hero.css for fade-in animation

--- project/src/Sections/Hero.jsx
+++ project/src/Sections/Hero.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import './Hero.css';
 
 function Hero () {
 
     return (
+        <div className="HeroAnimated">
         <div className="flex flex-col justify-center items-center h-screen">
         <h1 className="-mt-56 font-sans text-3xl sm:text-4xl md:text-5xl text-center text-white">I'm Haaris Rahmanyar, Please Check out My Works</h1>
         <button onClick={() => window.location.href='#Projects'} className="text-red-700 mt-10 w-40 h-10 border-2 text-red border-red-700 hover:text-white duration-300 hover:bg-red-700 duration-300">
@@ -11,6 +12,7 @@ function Hero () {
 
         </button>
 
+        </div>
         </div>
     )
 }
